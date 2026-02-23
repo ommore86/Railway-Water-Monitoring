0ed1bcb5df23912e80de48e03c14bcf2108dff0b
Om More <omgmore2005@gmail.com>
2026-02-23 22:49:48 +0530

    Created database schema for railway monitoring system

---
+++ database/schema.sql
@@ -0,0 +1,40 @@
+USE railway_water_monitor;
+
+CREATE TABLE stations (
+    station_id VARCHAR(20) PRIMARY KEY,
+    station_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE trains (
+    train_no VARCHAR(20) PRIMARY KEY,
+    train_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE coaches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    train_no VARCHAR(20),
+    coach_no VARCHAR(10),
+
+    UNIQUE(train_no, coach_no),
+
+    FOREIGN KEY (train_no) REFERENCES trains(train_no)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE water_levels (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    station_id VARCHAR(20),
+    train_no VARCHAR(20),
+    coach_no VARCHAR(10),
+
+    water_level INT,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    UNIQUE(train_no, coach_no),
+
+    FOREIGN KEY (station_id) REFERENCES stations(station_id) ON DELETE CASCADE,
+    FOREIGN KEY (train_no) REFERENCES trains(train_no) ON DELETE CASCADE
+);
+
+SHOW TABLES;
